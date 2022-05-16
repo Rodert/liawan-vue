28a2bb6a59f5f84c157b68581b70f1d1d58528a1
wangshiyu <15134966191@163.com>
2022-05-16 17:24:46 +0800

    backgroup gif

--- liawan-ui-admin/src/views/Register.vue
+++ liawan-ui-admin/src/views/Register.vue
@@ -151,7 +151,7 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
+  background-image: url("https://tva3.sinaimg.cn/large/007F3CC8ly1h2ad5zfsjrg30hr08qx6p.gif");
   background-size: cover;
 }
 .title {
